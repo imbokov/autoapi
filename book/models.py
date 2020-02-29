073c2d3b6552b24ab91cdbf6cd78f84cb32d8275
imbokov <tapycx@gmail.com>
2020-02-29 16:40:13 +0300

    test models setup

--- book/models.py
+++ book/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+
+class Author(Model):
+    first_name = CharField(max_length=255)
+    last_name = CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(Model):
+    author = ForeignKey(Author, on_delete=PROTECT)
+
+    title = CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+
+class Post(Model):
+    user = ForeignKey("auth.User", on_delete=PROTECT)
+
+    title = CharField(max_length=255)
+    text = TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(Model):
+    user = ForeignKey("auth.User", on_delete=PROTECT)
+    post = ForeignKey(Post, on_delete=PROTECT)
+
+    text = TextField()
+
+    def __str__(self):
+        return f"Comment {self.pk} by {self.user} for {self.post}"
